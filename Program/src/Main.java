d316ba2ccf657d7d7b5adc8d6bb4f375380d667c
André L Josefino <ALJosefino@gmail.com>
2024-07-16 23:38:04 -0300

    Just testing

---
+++ Program/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Cria uma instância da classe HelloWorld
+
+        HelloWorld helloWorld = new HelloWorld();
+
+        // Invoca o método sayHello() usando o obejto
+
+        // helloWorld para isso
+
+        helloWorld.sayHello();
+
+        }
+
+}
